c1018bf730eaff5701bd4bb0b22eddc06f18c27f
IsaacJorda <41235064+IsaacJorda@users.noreply.github.com>
2023-03-28 10:47:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+
+> Status do projeto: Em desenvolvimento
+
+Para rodar esse projeto na sua máquina, por favor digite:
+
+---
+node app.js
+---
